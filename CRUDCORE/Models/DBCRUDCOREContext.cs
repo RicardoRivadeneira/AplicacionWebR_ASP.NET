22db9a6f286c17bcba07eee6492eaeae75e07b34
RicardoRivadeneira <87491407+RicardoRivadeneira@users.noreply.github.com>
2023-11-27 01:40:38 -0500

    Add files via upload

---
+++ CRUDCORE/Models/DBCRUDCOREContext.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace CRUDCORE.Models
+{
+    public partial class DBCRUDCOREContext : DbContext
+    {
+        public DBCRUDCOREContext()
+        {
+        }
+
+        public DBCRUDCOREContext(DbContextOptions<DBCRUDCOREContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Cargo> Cargos { get; set; } = null!;
+        public virtual DbSet<Empleado> Empleados { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Data Source=(local);Initial Catalog=DBCRUDCORE;user id=sa;pwd=claveSQL4");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Cargo>(entity =>
+            {
+                entity.HasKey(e => e.IdCargo)
+                    .HasName("PK__CARGO__6C985625916B3823");
+
+                entity.ToTable("CARGO");
+
+                entity.Property(e => e.Descripcion)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Empleado>(entity =>
+            {
+                entity.HasKey(e => e.IdEmpleado)
+                    .HasName("PK__EMPLEADO__CE6D8B9EBA98B543");
+
+                entity.ToTable("EMPLEADO");
+
+                entity.Property(e => e.Correo)
+                    .HasMaxLength(60)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.NombreCompleto)
+                    .HasMaxLength(60)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Telefono)
+                    .HasMaxLength(60)
+                    .IsUnicode(false);
+
+                entity.HasOne(d => d.oCargo)
+                    .WithMany(p => p.Empleados)
+                    .HasForeignKey(d => d.IdCargo)
+                    .HasConstraintName("FK__EMPLEADO__IdCarg__398D8EEE");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
